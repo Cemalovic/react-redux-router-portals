e2e5a394dc3c8dc8f3c39e61dd9a5f7be77286f4
Milos Cemalovic <Milos.Cemalovic@rs.ey.com>
2022-05-05 16:59:53 +0200

    Added Modal/Dialog with createPortal method

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,10 @@
 import { Link } from 'react-router-dom'
+import Modal from './Modal'
 
 const Contact = () => {
   return (
     <div>
+      <Modal />
       <div
         className='ui raised very padded text container segment'
         style={{ marginTop: '80px' }}
