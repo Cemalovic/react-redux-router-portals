918ba80713fc4d8a2741f251444cac3bf36c48a1
Milos Cemalovic <Milos.Cemalovic@rs.ey.com>
2022-05-05 12:37:32 +0200

    Added semantic-ui in project

---
+++ src/components/Contact.js
@@ -0,0 +1,17 @@
+const Contact = () => {
+  return (
+    <div
+      className='ui raised very padded text container segment'
+      style={{ marginTop: '80px' }}
+    >
+      <h3 className='ui header'>Contact</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque natus
+        libero illum ducimus unde consectetur aspernatur accusantium harum
+        voluptas nostrum.
+      </p>
+    </div>
+  )
+}
+
+export default Contact
---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+const Home = () => {
+  return (
+    <div
+      className='ui raised very padded text container segment'
+      style={{ marginTop: '80px' }}
+    >
+      <h3 className='ui header'>Home</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque natus
+        libero illum ducimus unde consectetur aspernatur accusantium harum
+        voluptas nostrum.
+      </p>
+    </div>
+  )
+}
+
+export default Home
